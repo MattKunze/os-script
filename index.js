455c7eab62921a75e30abb8e03c0085970231f35
Matt Kunze <matt.kunze@datasplice.com>
2017-11-03 13:14:46 -0600

    spawn things in a cross-platform way

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let platform = require('os').platform();
 let script = process.argv[process.argv.length - 1];
 let spawn = require('child_process').spawn;
-let child = spawn('npm', ['run', `${script}:${platform}`]);
+let child = null;
+if(platform === 'win32') {
+  child = spawn('cmd', ['/c', 'npm', 'run', `${script}:${platform}`]);
+}
+else {
+  child = spawn('npm', ['run', `${script}:${platform}`]);
+}
 
 child.stdout.on('data', data => console.log(`${data}`));
 child.stderr.on('data', data => console.log(`${data}`));
